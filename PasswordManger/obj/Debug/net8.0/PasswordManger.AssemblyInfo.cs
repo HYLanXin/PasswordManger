904db9e8003e64ce6329e1604c42be63b1a2de24
岚信 <943289922@qq.com>
2025-12-23 22:54:05 +0800

    chore(build): 更新项目构建配置和包管理设置
    - 在AssemblyInfo中添加版本信息的git提交哈希
    - 配置NuGet包的备用文件夹路径
    - 添加Visual Studio的NuGet配置文件路径
    - 更新SDK分析级别到10.0.100
    - 修改运行时标识符图表路径到新版本
    - 在全局using语句中移除global限定符
    - 添加代码分析级别和启用代码样式严重性属性

--- PasswordManger/obj/Debug/net8.0/PasswordManger.AssemblyInfo.cs
+++ PasswordManger/obj/Debug/net8.0/PasswordManger.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PasswordManger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a736dd0bd25c5f1c2e381769187b1f42198bc5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PasswordManger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PasswordManger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
